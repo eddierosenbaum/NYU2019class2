66e58a3ef8c64e106b121373cadba9cab08d3d51
Eddie Rosenbaum <49243589+eddierosenbaum@users.noreply.github.com>
2019-04-16 21:36:47 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
 $('#alertButton').on('click', function() {
   alert('The Reform will take place Fall 2019');
+  <script src='https://static-assets.mapbox.com/gl-pricing/dist/mapbox-gl.js'></script>
+<link href='https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' rel='stylesheet' />
 });
