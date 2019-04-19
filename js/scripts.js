f831d13af1608be7aef71bada2aa0f19638baa68
eddierosenbaum <49243589+eddierosenbaum@users.noreply.github.com>
2019-04-18 21:18:08 -0400

    Finally got the map

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $('#alertButton').on('click', function() {
   alert('The Reform will take place Fall 2019');
-  <script src='https://static-assets.mapbox.com/gl-pricing/dist/mapbox-gl.js'></script>
-<link href='https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' rel='stylesheet' />
+});
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWRkaWVyb3NlbmJhdW0iLCJhIjoiY2p1a2hjYnN1MXZ5bDQzcGcyd3M2djkyNCJ9._8wwgcVBrEeXxMoTgrXRGg';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/eddierosenbaum/cjuklbw401zjh1fqueauiyv2y',
+center: [6.848748, 48.108150],
+zoom: 4.0
 });
